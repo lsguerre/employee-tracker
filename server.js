427b44dbeea69b756c6899f137e578d443e4493e
Opal <guerre5lily@gmail.com>
2022-04-07 13:30:00 -0700

    db query

--- server.js
+++ server.js
@@ -1,13 +1,33 @@
 const express = require('express')
+const mysql = require('mysql2')
 
 const PORT = process.env.PORT || 3001
 const app = express()
 
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
-  })
+
+app.use(express.urlencoded({ extended: false }))
+app.use(express.json())
+
+const db = mysql.createConnection(
+    {
+      host: 'localhost',
+      user: 'root',
+      password: 'da3m0nNugg3t$',
+      database: 'employeetracker'
+    },
+    console.log('Connected to the employeetracker database.')
+  )
+
+
+db.query(`SELECT * FROM employee`, (err, rows) => {
+  console.log(rows);
+})
 
 
   app.use((req, res) => {
-    res.status(404).end();
-  });
+    res.status(404).end()
+  })
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+  })
+
